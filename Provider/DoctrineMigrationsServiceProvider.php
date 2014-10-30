797195d5699a49d5e7c9f05d14138602cd858610
Grzegorz Korba <grzegorz.korba@codito.net>
2014-10-31 00:59:56 +0100

    Initial definition of service provider

---
+++ Provider/DoctrineMigrationsServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Codito\Silex\DoctrineMigrationsService\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Knp\Console\Application as ConsoleApp;
+use Knp\Console\ConsoleEvents;
+use Knp\Console\ConsoleEvent;
+use Doctrine\DBAL\Migrations\Tools\Console\Command as DoctrineCommands;
+
+class DoctrineMigrationsServiceProvider implements ServiceProviderInterface {
+	public function register(Application $app) {
+		$app['dispatcher']->addListener(ConsoleEvents::INIT, function(ConsoleEvent $event) {
+			$consoleApp = $event->getApplication(); /* @var $console ConsoleApp */
+			$consoleApp->add(new DoctrineCommands\StatusCommand());
+		});
+	}
+
+	public function boot(Application $app) {
+		
+	}
+}
